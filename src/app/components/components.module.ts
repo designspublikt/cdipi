8a96a5782643ba0f2a74b47249b1a7cbdf954bfe
Nueva Mente <dsandovalnm@gmail.com>
2022-03-02 22:15:06 -0300

    Desarrollo Home

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,28 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CategoriesBarComponent } from './categories-bar/categories-bar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    SearchBarComponent,
+    CategoriesBarComponent,
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    NavbarComponent,
+    SearchBarComponent,
+    CategoriesBarComponent
   ]
 })
 export class ComponentsModule { }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ComponentsModule } from '../components/components.module';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    NotfoundComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule,
+    RouterModule
+  ],
+  exports: [
+    HomeComponent,
+    NotfoundComponent,
+    RouterModule
   ]
 })
 export class PagesModule { }
